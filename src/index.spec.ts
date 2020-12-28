f69dbda7e3538f341ead7aa97bd5352501c57f04
Benoît Eveillard <benoit.eveillard@gmail.com>
2020-12-28 09:48:52 -0500

    feature(diamond) implement

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,32 @@
-import { systemUnderTest } from ".";
+import { diamond, printLine, printChars } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+describe("Test of diamond()", function () {
+  it("Should return '* * '", function () {
+    expect(printChars('*', 2)).toEqual("* * ");
   });
+
+  it("Should return '* '", function () {
+    expect(printLine(1, 1)).toEqual("* \n");
+  });
+
+  it("Should return '  *   '", function () {
+    expect(printLine(1, 3)).toEqual("  *   \n");
+  });
+
+  it("Should return a diamond of size 0", function () {
+    expect(diamond(0)).toEqual("");
+  });
+
+  it("Should return a diamond of size 1", function () {
+    expect(diamond(1)).toEqual("* \n");
+  });
+
+  it("Should return a diamond of size 3", function () {
+    expect(diamond(3)).toEqual("  *   \n* * * \n  *   \n");
+  });
+
+  it("Should return a diamond of size 7", function () {
+    expect(diamond(7)).toEqual("      *       \n    * * *     \n  * * * * *   \n* * * * * * * \n  * * * * *   \n    * * *     \n      *       \n");
+  });
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-export function systemUnderTest(): boolean {
-  return true;
+export function diamond(n) {
+  let diamond = "";
+
+  for (let lineSize = 1; lineSize <= n; lineSize += 2)     diamond += printLine(lineSize, n);
+  for (let lineSize = n - 2; lineSize > 0; lineSize -= 2)  diamond += printLine(lineSize, n);
+
+  console.log(diamond);
+
+  return diamond;
+}
+
+export function printLine(lineSize, diamondSize) {
+  let blanks = printChars(' ', (diamondSize - lineSize) / 2);
+  let stars = printChars('*', lineSize);
+  return blanks + stars + blanks + "\n";
+}
+
+export function printChars(char, length) {
+  let string = "";
+
+  for (let i = 0; i < length; i++) {
+    string += char + " ";
+  }
+
+  return string;
 }
